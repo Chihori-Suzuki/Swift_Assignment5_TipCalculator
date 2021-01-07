f96c6845b2401741d86b143232d722ccb6b9918d
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-01-07 13:51:15 -0800

    first commit

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var totalBillLabel: UILabel!
+    @IBOutlet var calculateBillBtn: UIButton!
+    @IBOutlet var billAmountTextField: UITextField!
+    @IBOutlet var tipSlider: UISlider!
+    @IBOutlet var tipAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+
     }
+    @IBAction func calculateBill(_ sender: Any) {
+        let tipVal = tipSlider.value
+        guard var total = Double(billAmountTextField.text!) else {
+            return
+        }
+        total = total + (Double(tipVal) * total / 100)
+        totalBillLabel.text = String(total)
 
-
+    }
+    @IBAction func perChanged(_ sender: Any) {
+        tipAmountLabel.text = String(tipSlider.value)
+    }
+    
 }
-
