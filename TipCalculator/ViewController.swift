a291ff2bd98f7e6e737fbe39660699b6ff0f1927
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-01-07 23:18:42 -0800

    first commit

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -20,19 +20,27 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
-
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard(_:)))
+        view.addGestureRecognizer(gestureRecognizer)
+        
     }
+    @objc func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+      view.endEditing(true)
+    }
+    
     @IBAction func calculateBill(_ sender: Any) {
         let tipVal = tipSlider.value
         guard var total = Double(billAmountTextField.text!) else {
             return
         }
-        total = total + (Double(tipVal) * total / 100)
-        totalBillLabel.text = String(total)
-
+        total = total + (Double(tipVal) * total)
+        totalBillLabel.text = "$\(String(format: "%.2f", total))"
     }
+    
     @IBAction func perChanged(_ sender: Any) {
-        tipAmountLabel.text = String(tipSlider.value)
+        print(tipSlider.value)
+        tipAmountLabel.text = "\(String(format: "%.0f", tipSlider.value * 100))%"
+        
     }
     
 }
